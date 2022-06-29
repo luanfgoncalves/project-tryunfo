b5442db0471efc36ec19db7316173f6b98161968
luanfgoncalves <luanfgoncalves@outlook.com>
2022-06-29 13:54:53 -0300

    FEAT: labels para os inputs do formulário em Forms.jsx

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -4,21 +4,35 @@ import React from 'react';
 class Form extends React.Component {
   render() {
     return (
-      <div>
-        <input type="text" data-testid="name-input" />
-        <textarea data-testid="description-input" />
-        <input type="number" data-testid="attr1-input" />
-        <input type="number" data-testid="attr2-input" />
-        <input type="number" data-testid="attr3-input" />
-        <input type="text" data-testid="image-input" />
+      <form>
+        <label htmlFor="name-input">
+          <input type="text" data-testid="name-input" />
+        </label>
+        <label htmlFor="description-input">
+          <textarea data-testid="description-input" />
+        </label>
+        <label htmlFor="attr1-input">
+          <input type="number" data-testid="attr1-input" />
+        </label>
+        <label htmlFor="attr2-input">
+          <input type="number" data-testid="attr2-input" />
+        </label>
+        <label htmlFor="attr3-input">
+          <input type="number" data-testid="attr3-input" />
+        </label>
+        <label htmlFor="image-input">
+          <input type="text" data-testid="image-input" />
+        </label>
         <select data-testid="rare-input">
           <option value="normal">normal</option>
           <option value="raro">raro</option>
           <option value="muito raro">muito raro</option>
         </select>
-        <input type="checkbox" data-testid="trunfo-input" />
-        <button type="submit" data-testid="save-button"> Me clique :3 </button>
-      </div>
+        <label htmlFor="trunfo-input">
+          <input type="checkbox" data-testid="trunfo-input" />
+        </label>
+        <button type="submit" data-testid="save-button">Salvar</button>
+      </form>
     );
   }
 }
