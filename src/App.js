19fa70a4ea083eb607143e51ffd767e18e456c78
luanfgoncalves <luanfgoncalves@outlook.com>
2022-07-01 16:36:34 -0300

    FEAT: State em App.js; FIX: correções dos nomes de props e estados em App.js e Form.jsx.

--- src/App.js
+++ src/App.js
@@ -3,11 +3,58 @@ import Form from './components/Form';
 // import Card from './components/Card';
 
 class App extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      cardName: '',
+      cardDescription: '',
+      cardAttr1: '',
+      cardAttr2: '',
+      cardAttr3: '',
+      cardImage: '',
+      cardRare: '',
+      cardTrunfo: 'false', // boll
+      // hasTrunfo: 'false',
+      isSaveButtonDisabled: () => {},
+      onSaveButtonClick: () => {},
+    };
+  }
+
+// onInputChange();
+
   render() {
+    const {
+      cardName,
+      cardDescription,
+      cardAttr1,
+      cardAttr2,
+      cardAttr3,
+      cardImage,
+      cardRare,
+      cardTrunfo,
+      // hasTrunfo,
+      isSaveButtonDisabled,
+      onInputChange,
+      onSaveButtonClick,
+    } = this.state;
     return (
       <div>
         <h1>Tryunfo</h1>
-        <Form />
+        <Form
+          cardName={ cardName }
+          cardDescription={ cardDescription }
+          cardAttr1={ cardAttr1 }
+          cardAttr2={ cardAttr2 }
+          cardAttr3={ cardAttr3 }
+          cardImage={ cardImage }
+          cardRare={ cardRare }
+          cardTrunfo={ cardTrunfo }
+          hasTrunfo={ hasTrunfo }
+          isSaveButtonDisabled={ isSaveButtonDisabled }
+          onInputChange={ this.onInputChange }
+          onSaveButtonClick={ onSaveButtonClick }
+        />
         {/* <Card /> */}
       </div>
     );
@@ -15,3 +62,6 @@ class App extends React.Component {
 }
 
 export default App;
+
+// Referêcias:
+// https://www.codegrepper.com/code-examples/javascript/oninputchange+react
--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -7,9 +7,9 @@ class Form extends React.Component {
     const {
       cardName,
       cardDescription,
-      cartAttr1,
-      cartAttr2,
-      cartAttr3,
+      cardAttr1,
+      cardAttr2,
+      cardAttr3,
       cardImage,
       cardRare,
       cardTrunfo,
@@ -26,6 +26,7 @@ class Form extends React.Component {
         <label htmlFor="name-input">
           <p>Nome</p>
           <input
+            placeholder="Nome da carta"
             name="cardName"
             id="name-input"
             type="text"
@@ -38,6 +39,7 @@ class Form extends React.Component {
         <label htmlFor="description-input">
           <p>Descrição</p>
           <textarea
+            placeholder="Descrição da carta"
             name="cardDescription"
             id="description-input"
             maxLength="100"
@@ -50,11 +52,12 @@ class Form extends React.Component {
         <label htmlFor="attr1-input">
           Attr01
           <input
-            name="cartAttr1"
+            placeholder="Primeiro atributo"
+            name="cardAttr1"
             id="attr1-input"
             type="number"
             data-testid="attr1-input"
-            value={ cartAttr1 }
+            value={ cardAttr1 }
             onChange={ onInputChange }
           />
         </label>
@@ -62,11 +65,12 @@ class Form extends React.Component {
         <label htmlFor="attr2-input">
           Attr02
           <input
-            name="cartAttr2"
+            placeholder="Segundo atributo"
+            name="cardAttr2"
             id="attr2-input"
             type="number"
             data-testid="attr2-input"
-            value={ cartAttr2 }
+            value={ cardAttr2 }
             onChange={ onInputChange }
           />
         </label>
@@ -74,11 +78,12 @@ class Form extends React.Component {
         <label htmlFor="attr3-input">
           Attr03
           <input
-            name="cartAttr3"
+            placeholder="Terceiro atributo"
+            name="cardAttr3"
             id="attr3-input"
             type="number"
             data-testid="attr3-input"
-            value={ cartAttr3 }
+            value={ cardAttr3 }
             onChange={ onInputChange }
           />
         </label>
@@ -86,6 +91,7 @@ class Form extends React.Component {
         <label htmlFor="image-input">
           Imagem
           <input
+            placeholder="imagem da carta"
             name="cardImage"
             id="image-input"
             type="text"
@@ -112,6 +118,7 @@ class Form extends React.Component {
         <label htmlFor="trunfo-input">
           Super Trybe Trunfo
           <input
+            placeholder="Trunfo"
             name="cardTrunfo"
             id="trunfo-input"
             type="checkbox"
@@ -139,9 +146,9 @@ class Form extends React.Component {
 Form.propTypes = {
   cardName: propTypes.string.isRequired,
   cardDescription: propTypes.string.isRequired,
-  cartAttr1: propTypes.string.isRequired,
-  cartAttr2: propTypes.string.isRequired,
-  cartAttr3: propTypes.string.isRequired,
+  cardAttr1: propTypes.string.isRequired,
+  cardAttr2: propTypes.string.isRequired,
+  cardAttr3: propTypes.string.isRequired,
   cardImage: propTypes.string.isRequired,
   cardRare: propTypes.string.isRequired,
   cardTrunfo: propTypes.bool.isRequired,
