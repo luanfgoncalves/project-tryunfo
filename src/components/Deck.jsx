1d130dc219426a8ff722ab612d06c9faf721b91a
luanfgoncalves <luanfgoncalves@outlook.com>
2022-07-05 10:56:41 -0300

    FIX: comenta os elementos em Deck.jsx enquanto ele não fica pronto.

--- src/components/Deck.jsx
+++ src/components/Deck.jsx
@@ -1,36 +1,36 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import Card from './Card';
+// import React from 'react';
+// import PropTypes from 'prop-types';
+// import Card from './Card';
 
-class Deck extends React.Component {
-  render() {
-    return (
-    <div>
-    { cardData.map((element) => (
-      <Card
-      key={ element.cardName }
-      cardName={ element.cardName }
-      cardDescription={ element.cardDescription }
-      cardAttr1={ element.cardAttr1 }
-      cardAttr2={ element.cardAttr2 }
-      cardAttr3={ element.cardAttr3 }
-      cardRare={ element.cardRare }
-      cardTrunfo={ element.cardTrunfo }
-      cardImage={ element.cardImage }
-    />
-    ))}
-    </div>
-  )};
+// class Deck extends React.Component {
+//   render() {
+//     return (
+//     <div>
+//     { cardData.map((element) => (
+//       <Card
+//       key={ element.cardName }
+//       cardName={ element.cardName }
+//       cardDescription={ element.cardDescription }
+//       cardAttr1={ element.cardAttr1 }
+//       cardAttr2={ element.cardAttr2 }
+//       cardAttr3={ element.cardAttr3 }
+//       cardRare={ element.cardRare }
+//       cardTrunfo={ element.cardTrunfo }
+//       cardImage={ element.cardImage }
+//     />
+//     ))}
+//     </div>
+//   )};
 
-Deck.propTypes = {
-  cardName: PropTypes.string.isRequired,
-  cardDescription: PropTypes.string.isRequired,
-  cardAttr1: PropTypes.string.isRequired,
-  cardAttr2: PropTypes.string.isRequired,
-  cardAttr3: PropTypes.string.isRequired,
-  cardImage: PropTypes.string.isRequired,
-  cardRare: PropTypes.string.isRequired,
-  cardTrunfo: PropTypes.bool.isRequired,
-};
+// Deck.propTypes = {
+//   cardName: PropTypes.string.isRequired,
+//   cardDescription: PropTypes.string.isRequired,
+//   cardAttr1: PropTypes.string.isRequired,
+//   cardAttr2: PropTypes.string.isRequired,
+//   cardAttr3: PropTypes.string.isRequired,
+//   cardImage: PropTypes.string.isRequired,
+//   cardRare: PropTypes.string.isRequired,
+//   cardTrunfo: PropTypes.bool.isRequired,
+// };
 
-export default Deck;
+// export default Deck;
