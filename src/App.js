860f817043e2079196f48d405193acad159071dd
luanfgoncalves <luanfgoncalves@outlook.com>
2022-07-05 07:05:04 -0300

    FIX: Reestabelece saveButtonManager() em App.js, o estado correpondente e suas declarações para o requisito 5 funcionar - VQV!!!!!!

--- src/App.js
+++ src/App.js
@@ -14,16 +14,55 @@ class App extends React.Component {
       cardAttr3: '',
       cardRare: 'normal',
       cardTrunfo: false,
+      saveButton: true,
     };
+  }
+
+  saveButtonManager = () => {
+    const { cardName,
+      cardDescription,
+      cardAttr1,
+      cardAttr2,
+      cardAttr3,
+      cardImage,
+      cardRare,
+    } = this.state;
+
+    const cardNameVar = cardName !== ''
+    && cardDescription !== ''
+    && cardRare !== '' && cardImage !== '';
+
+    const maxAtributeValue = 90;
+    const maxAtributeSum = 210;
+
+    const atribute1Value = Number(cardAttr1) >= 0
+    && Number(cardAttr1) <= maxAtributeValue;
+
+    const atribute2Value = Number(cardAttr2) >= 0
+    && Number(cardAttr2) <= maxAtributeValue;
+
+    const atribute3Value = Number(cardAttr3) >= 0
+    && Number(cardAttr3) <= maxAtributeValue;
+
+    const attrSum = Number(cardAttr1)
+    + Number(cardAttr2)
+    + Number(cardAttr3) <= maxAtributeSum;
+
+    this.setState({
+      saveButton: !(cardNameVar
+        && atribute1Value
+        && atribute2Value
+        && atribute3Value
+        && attrSum) });
   }
 
   onInputChange = (event) => {
     const { target } = event;
     const { name } = target;
     const value = target.type === 'checkbox' ? target.checked : target.value;
-    this.setState({
+    this.setState(({
       [name]: value,
-    });
+    }), () => this.saveButtonManager());
   };
 
   render() {
@@ -35,6 +74,7 @@ class App extends React.Component {
       cardAttr3,
       cardRare,
       cardTrunfo,
+      saveButton,
     } = this.state;
 
     return (
@@ -51,6 +91,7 @@ class App extends React.Component {
           cardRare={ cardRare }
           cardTrunfo={ cardTrunfo }
           onInputChange={ this.onInputChange }
+          isSaveButtonDisabled={ saveButton }
         />
 
         <Card
